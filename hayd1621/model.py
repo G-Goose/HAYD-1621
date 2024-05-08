cc3c341c05a8ab1cbb5b1a04d289ac36585d5f4f
Gregory Chapron <gchapron@gmail.com>
2024-05-08 18:05:28 +0200

    updated model.py file

--- hayd1621/model.py
+++ hayd1621/model.py
@@ -1,8 +1,34 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+from keras import img_to_array, preprocess_input
+from tensorflow import tile
+from tensorflow import expand_dims
 
-def evaluate():
-    return 0.99
+def loading_model():
+    path = "/Users/gregory/code/G-Goose/HAYD-1621/test_trained_model"
+    model = tf.keras.models.load_model(path)
+    return model
+
+def input_process(pict):
+    pict.resize((254, 254))
+    pict = img_to_array(pict)
+    pict = tile(pict, [1, 1, 3])
+    pict = expand_dims(pict, axis=0)
+    preproc_pict = preprocess_input(pict)
+    return preproc_pict
+
+def pred(preproc_pict):
+    model = loading_model()
+    y_pred = model.predict(preproc_pict)
+    return y_pred # check the output format
+
+# model = loading_model()
+#    X_processed = input_process(X_pred)
+#    y_pred = model.predict(X_processed)
+
+# def evaluate():
+#    return 0.99
 
 if __name__ == '__main__':
     evaluate()
