e5a1bfdd910da9161fb70764aec5c8770636225c
Ali Kalantarnia <ali.kalantarnia@mail.de>
2024-05-13 12:28:06 +0200

    adds functions for api and bq outputs

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
-from hayd1621.model import loading_model, input_process, pred # changed
+from hayd1621.model import output_api, pred, loading_model, input_process
 import tensorflow as tf
 from PIL import Image
 
@@ -12,14 +12,6 @@ app = FastAPI()
 app.state.model = loading_model()
 
 
-# # Allow all requests (optional, good for development purposes)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allows all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Allows all methods
-#     allow_headers=["*"],  # Allows all headers
-# )
 
 @app.get("/")
 def index():
@@ -28,21 +20,11 @@ def index():
 @app.post('/upload_your_nice_face')
 async def receive_image(img: UploadFile):
     ### Receiving and processing the image
-    # contents = await img.read()
     request_object_content = await img.read()
     img = Image.open(io.BytesIO(request_object_content))
-    # img = tf.keras.preprocessing.image.img_to_array(contents)
     print('* * * before-processing completed * * *')
-    processed_face = input_process(img)
-    print('* * * processing completed * * *')
 
     ### Predicting the emotion
-    class_names = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-    predicted_emotion = pred(preproc_pict = processed_face, model = app.state.model)
-    output_list = np.round(predicted_emotion, 2)*100
-    output = dict(zip(class_names, output_list.flatten().tolist()))
+    predicted_emotion = output_api(pred(preproc_pict = input_process(img), model = app.state.model))
 
-
-    ### Returning the predicted emotion
-    print(output)
-    return output
+    return predicted_emotion
