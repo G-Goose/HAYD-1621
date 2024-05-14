f34978c6d1b231d4eb92f33cdc697415b910a71c
Ali Kalantarnia <ali.kalantarnia@mail.de>
2024-05-14 15:15:44 +0200

    functionality added: Save data to BQ

--- api/fast.py
+++ api/fast.py
@@ -1,30 +1,26 @@
-from fastapi import FastAPI, UploadFile, File
-from hayd1621.model import output_api, pred, loading_model, input_process
-import tensorflow as tf
-from PIL import Image
-import logging
-
-import numpy as np
-import io
+from fastapi import FastAPI, UploadFile
+from hayd1621.model import output_api, output_bq, pred, loading_model, input_process
+from bq import bq
 
 app = FastAPI()
 
 app.state.model = loading_model()
-
-
 
 @app.get("/")
 def index():
     return {"status": "ok"}
 
 @app.post('/upload_your_nice_face')
-async def receive_image(img: UploadFile):
+async def mood_percent(img: UploadFile):
     ### Receiving and processing the image
-    print('* * * before-processing completed * * *')
     processed_face = input_process(img.file)
-    print('* * * processing completed * * *')
 
     ### Predicting the emotion
     predicted_emotion = output_api(pred(preproc_pict = processed_face, model = app.state.model))
 
     return predicted_emotion
+
+@app.get('/save_to_bq')
+def save_to_bq(val):
+    result = bq.update_or_insert_value(new_value=val)
+    return {"response": result}
